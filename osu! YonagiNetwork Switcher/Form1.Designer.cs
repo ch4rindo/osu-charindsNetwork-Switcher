aa7ccc7907a0ec7cafb14b8f7153a44d9be0f282
ch4rindo <103165856+ch4rindo@users.noreply.github.com>
2022-08-01 11:42:41 +0900

    First upload

---
+++ osu! YonagiNetwork Switcher/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace osu__YonagiNetwork_Switcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverIp = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Impact", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(12, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(336, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apply settings";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Impact", 22.75F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server IP:";
+            this.label1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // serverIp
+            // 
+            this.serverIp.Location = new System.Drawing.Point(137, 23);
+            this.serverIp.MaxLength = 15;
+            this.serverIp.Name = "serverIp";
+            this.serverIp.Size = new System.Drawing.Size(211, 19);
+            this.serverIp.TabIndex = 4;
+            this.serverIp.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkBox1.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.checkBox1.Location = new System.Drawing.Point(53, 51);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(296, 24);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "上記のIPにサーバーを変更する";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(361, 151);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.serverIp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "osu! YonagiNetwok Switcher v1.0.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverIp;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ osu! YonagiNetwork Switcher/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Net;
+
+namespace osu__YonagiNetwork_Switcher
+{
+    public partial class Form1 : Form
+    {
+
+        private Encoding enc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            remove_hosts_data();
+            bool r = true;
+            if (checkBox1.Checked) 
+            {
+                bool result = add_hosts_data();
+                if (!result)
+                {
+                    r = false;
+                }
+            }
+
+            if (r)
+            {
+                MessageBox.Show("サーバーの変更に成功しました。\r\nosuを再起動してください", "Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private bool add_hosts_data()
+        {
+            enc = Encoding.GetEncoding("UTF-8");
+            string path = @"c:/Windows/System32/drivers/etc/hosts";
+
+            if (!System.IO.File.Exists(path))
+            {
+                FileInfo fileInfo = new FileInfo(path);
+                FileStream fileStream = fileInfo.Create();
+                fileStream.Close();
+            }
+
+            //ファイルの内容を読み込む
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift_JIS"));
+
+            //内容をすべて読み込む
+            string s = sr.ReadToEnd();
+
+            //閉じる
+            sr.Close();
+
+            // 文字列置換
+            string host = serverIp.Text;
+            IPAddress ipaddr;
+            bool ret = IPAddress.TryParse(host, out ipaddr);
+            if (!ret)
+            {
+                MessageBox.Show("有効なIPアドレスを入力してください", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            s += "\r\n" + host + " osu.ppy.sh";
+            s += "\r\n" + host + " c.ppy.sh";
+            s += "\r\n" + host + " c1.ppy.sh";
+            s += "\r\n" + host + " c2.ppy.sh";
+            s += "\r\n" + host + " c3.ppy.sh";
+            s += "\r\n" + host + " c4.ppy.sh";
+            s += "\r\n" + host + " c5.ppy.sh";
+            s += "\r\n" + host + " c6.ppy.sh";
+            s += "\r\n" + host + " ce.ppy.sh";
+            s += "\r\n" + host + " a.ppy.sh";
+            s += "\r\n" + host + " i.ppy.sh";
+            s += "\r\n" + host + " i-need-to.click";
+            s += "\r\n" + host + " c.i-need-to.click";
+            s += "\r\n" + host + " i.i-need-to.click";
+            s += "\r\n" + host + " a.i-need-to.click";
+            s += "\r\n" + host + " old.i-need-to.click";
+
+            //Shift JISで書き込む
+            //書き込むファイルが既に存在している場合は、上書きする
+            StreamWriter sw = new StreamWriter(
+                path,
+                false,
+                Encoding.GetEncoding("Shift_JIS"));
+            //TextBox1.Textの内容を書き込む
+            sw.Write(s);
+            //閉じる
+            sw.Close();
+            return true;
+        }
+
+        private void remove_hosts_data()
+        {
+            enc = Encoding.GetEncoding("UTF-8");
+            string path = @"c:/Windows/System32/drivers/etc/hosts";
+
+            if (!System.IO.File.Exists(path))
+            {
+                FileInfo fileInfo = new FileInfo(path);
+                FileStream fileStream = fileInfo.Create();
+                fileStream.Close();
+            }
+
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift_JIS"));
+            //内容をすべて読み込む
+            string s = sr.ReadToEnd();
+
+            //閉じる
+            sr.Close();
+
+            // 文字列置換
+            /*
+            string host = "160.251.72.25";
+            s = s.Replace("\r\n"+host+" osu.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c1.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c2.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c3.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c4.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c5.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" c6.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" ce.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" a.ppy.sh", "");
+            s = s.Replace("\r\n"+host+" i.ppy.sh", "");
+            s = s.Replace("\r\n" + host + " i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " c.i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " i.i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " a.i-need-to.click", "");
+            s = s.Replace("\r\n" + host + " old.i-need-to.click", "");
+            */
+
+            //Shift JISで書き込む
+            //書き込むファイルが既に存在している場合は、上書きする
+            StreamWriter sw = new StreamWriter(
+                path,
+                false,
+                Encoding.GetEncoding("Shift_JIS"));
+            //TextBox1.Textの内容を書き込む
+            sw.Write("");
+            //閉じる
+            sw.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serverIp.Text = "113.144.214.47";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
